5d14f0bd9396775f8a8e014d46f66a65ca264fa7
Stuart Holland <soulc3@gmail.com>
2013-04-23 03:55:40 +0100

    Initial Horn of Valere testing and promotion addition.

---
+++ WoTMod/Lua/HornOfValereTest.lua
@@ -0,0 +1,34 @@
+-- HornOfValereTest
+-- Author: S3rgeus
+-- DateCreated: 4/23/2013 2:16:58 AM
+--------------------------------------------------------------
+
+print("Test code for Horn of Valere functionality running.")
+print("This should not be active in deployed builds.")
+
+function testHornPlacement(playerID)
+	print("Mod starting...")
+	if (playerID == 0) then
+		print("Found player 1...")
+		local pPlayer = Players[playerID]
+		local pCity = pPlayer:GetCapitalCity()
+
+		print("Got capital city")
+
+		local X = pCity:GetX()
+		local Y = pCity:GetY()
+
+		X = X - 1;
+
+		print("Placing Horn at " .. X .. ", " .. Y .. ".")
+
+		Map.PlaceHornOfValere(X, Y);
+
+		print("done!")
+	end
+end
+
+function HornHandler(playerID)
+	print(pcall(testHornPlacement, playerID))
+end
+GameEvents.PlayerDoTurn.Add(HornHandler);
