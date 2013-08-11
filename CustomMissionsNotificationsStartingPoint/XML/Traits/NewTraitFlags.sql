99a1628bab726391421932a49742146db0f1be16
Stuart Holland <soulc3@gmail.com>
2013-08-11 19:16:20 +0100

    Added remaining leaders. Fixed missing DB actions.
    Added Imasharard of the Numydians and Tvet of the Svestans, including
    relevant text keys and pedia placeholders.
    
    Fixed some incorrectly/not included XML entries into the game DB.

--- CustomMissionsNotificationsStartingPoint/XML/Traits/NewTraitFlags.sql
+++ CustomMissionsNotificationsStartingPoint/XML/Traits/NewTraitFlags.sql
@@ -1,4 +1,4 @@
-ALTER TABLE Traits ADD DoubleUnits boolean DEFAULT false;
+ALTER TABLE Traits ADD ExtraUnitsWhenTrained integer DEFAULT 0;
 ALTER TABLE Traits ADD TradeStopsWars boolean DEFAULT false;
 ALTER TABLE Traits ADD ReqTradeRoutesForPeace integer DEFAULT -1;
 ALTER TABLE Traits ADD PuppetsReligiousFollowers boolean DEFAULT false;
