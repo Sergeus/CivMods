caad56a4cba5efa8b9ef549e6e226a3d120ffc86
Stuart Holland <soulc3@gmail.com>
2013-08-25 21:22:33 +0100

    Fixed a bug in trade route yield caused by stupid code ordering.

--- CvGameCoreDLL_Expansion2/CvTradeClasses.cpp
+++ CvGameCoreDLL_Expansion2/CvTradeClasses.cpp
@@ -1787,13 +1787,30 @@ int CvPlayerTrade::GetTradeConnectionBaseValueTimes100(const TradeConnection& kT
 	{
 		if (GC.getGame().GetGameTrade()->IsConnectionInternational(kTradeConnection))
 		{
+			// ----------------------------------------------------------------
+			// SiegeMod Addition
+			// ----------------------------------------------------------------
+			CvCity* pCity = GC.getMap().plot(kTradeConnection.m_iOriginX, kTradeConnection.m_iOriginY)->getPlotCity();
+
+			int iResult = 0;
+
+			for (int i = 0; i < GC.getNumBuildingInfos(); i++)
+			{
+				const BuildingTypes eBuilding = static_cast<BuildingTypes>(i);
+				CvBuildingEntry* pkBuildingInfo = GC.getBuildingInfo(eBuilding);
+
+				if (pCity->GetCityBuildings()->GetNumBuilding(eBuilding) > 0)
+				{
+					iResult += pkBuildingInfo->GetTradeRouteYieldChange(kTradeConnection.m_eDomain, eYield);
+				}
+			}
+
 			if (eYield == YIELD_GOLD)
 			{
-				int iResult = 0;
 				if (!GET_PLAYER(kTradeConnection.m_eDestOwner).isMinorCiv())
 				{
 					int iBase = GC.getINTERNATIONAL_TRADE_BASE();
-					iResult = iBase;
+					iResult += iBase;
 				}
 				return iResult;
 			}
@@ -1807,30 +1824,10 @@ int CvPlayerTrade::GetTradeConnectionBaseValueTimes100(const TradeConnection& kT
 					iAdjustedTechDifference = max(iCeilTechDifference, 1);
 				}
 
-				return iAdjustedTechDifference * 100;
+				return (iAdjustedTechDifference * 100) + (iResult * 100);
 			}
-			// ----------------------------------------------------------------
-			// SiegeMod Addition
-			// ----------------------------------------------------------------
-			else
-			{
-				CvCity* pCity = GC.getMap().plot(kTradeConnection.m_iOriginX, kTradeConnection.m_iOriginY)->getPlotCity();
 
-				int iResult = 0;
-
-				for (int i = 0; i < GC.getNumBuildingInfos(); i++)
-				{
-					const BuildingTypes eBuilding = static_cast<BuildingTypes>(i);
-					CvBuildingEntry* pkBuildingInfo = GC.getBuildingInfo(eBuilding);
-
-					if (pCity->GetCityBuildings()->GetNumBuilding(eBuilding) > 0)
-					{
-						iResult += pkBuildingInfo->GetTradeRouteYieldChange(kTradeConnection.m_eDomain, eYield);
-					}
-				}
-
-				return iResult * 100;
-			}
+			return iResult * 100;
 		}
 	}
 	else
