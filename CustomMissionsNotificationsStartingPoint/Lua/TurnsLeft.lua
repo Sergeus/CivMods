23803bd27763772ace894a257bc08a1457e2f33c
Stuart Holland <soulc3@gmail.com>
2013-09-01 20:59:42 +0100

    Added new city names for AI civs. Added AI behavior for Numydia to expand after a certain amount of time in the scenario. Values ready for similar behavior for Argast and Cyats.

--- CustomMissionsNotificationsStartingPoint/Lua/TurnsLeft.lua
+++ CustomMissionsNotificationsStartingPoint/Lua/TurnsLeft.lua
@@ -26,14 +26,52 @@ gArgastFortCoords = {
 	},
 }
 
+gNumydiaExpansionCoords = {
+	["X"] = 24,
+	["Y"] = 20,
+}
+
+gCyatsExpansionCoords = {
+	[0] = {
+		["X"] = 58,
+		["Y"] = 28,
+	},
+	[1] = {
+		["X"] = 61,
+		["Y"] = 33,
+	},
+	[2] = {
+		["X"] = 59,
+		["Y"] = 38,
+	},
+}
+
+gArgastExpansionCoords = {
+	[0] = {
+		["X"] = 18,
+		["Y"] = 40,
+	},
+	[1] = {
+		["X"] = 13,
+		["Y"] = 40,
+	},
+}
+
 gArgastReinforcementsFrequency = GameInfo.SiegeModConstants.SIEGEMOD_ARGAST_REINFORCEMENT_FREQUENCY.Value
 gArgastReinforcementsProbability = GameInfo.SiegeModConstants.SIEGEMOD_ARGAST_REINFORCEMENT_PROBABILITY.Value
 gArgastDeclareWarEarliest = GameInfo.SiegeModConstants.SIEGEMOD_ARGAST_DECLARE_WAR_EARLIEST.Value
 gArgastDenouncePlayerTurn = GameInfo.SiegeModConstants.SIEGEMOD_ARGAST_DENOUNCE_PLAYER_NOT_AT_WAR.Value
 gNumydiaDenounceArgastTurn = GameInfo.SiegeModConstants.SIEGEMOD_NUMYDIA_DENOUNCE_ARGAST_NOT_AT_WAR_TURN.Value
+gNumydiaExpansionTurn = GameInfo.SiegeModConstants.SIEGEMOD_NUMYDIA_EXPANSION_TURN.Value
+gCyatsExpansionTurnFirst = GameInfo.SiegeModConstants.SIEGEMOD_CYATS_EXPANSION_TURN_FIRST.Value
+gCyatsExpansionTurnSecond = GameInfo.SiegeModConstants.SIEGEMOD_CYATS_EXPANSION_TURN_SECOND.Value
+gCyatsExpansionTurnThird = GameInfo.SiegeModConstants.SIEGEMOD_CYATS_EXPANSION_TURN_THIRD.Value
 
 gArgastHasDenouncedSvesta = false
 gNumydiaHasDenouncedArgast = false
+
+gArgastNextExpansion = 0
+gCyatsNextExpansion = 0
 
 gArgastReinforcements = {
 	[0] = GameInfoTypes.UNIT_ARGAST_RAIDERS,
@@ -153,8 +191,42 @@ function NumydiaActions(playerID)
 		pNumydia:DoForceDenounce(pArgast:GetID())
 		gNumydiaHasDenouncedArgast = true
 	end
+
+	if currentTurn == gNumydiaExpansionTurn then
+		print("Numydia is now expanding...")
+
+		print("Founding city...")
+		pNumydia:Found(gNumydiaExpansionCoords.X, gNumydiaExpansionCoords.Y)
+
+		print("Giving Numydia another worker for their new city...")
+		pNumydia:InitUnit(GameInfoTypes.UNIT_WORKER, gNumydiaExpansionCoords.X, gNumydiaExpansionCoords.Y)
+
+		Players[Game.GetActivePlayer()]:AddNotification(GameInfoTypes.NOTIFICATION_NUMYDIA_EXPANDED, nil, nil, gNumydiaExpansionCoords.X, gNumydiaExpansionCoords.Y, GameInfoTypes.CIVILIZATION_NUMYDIA)
+	end
 end
 GameEvents.PlayerDoTurn.Add(NumydiaActions)
+
+function CyatsActions(playerID)
+	if playerID ~= Game.GetActivePlayer() then
+		return
+	end
+
+	print("Managing the Cyats actions...")
+
+	local pCyats
+
+	for i, pPlayer in pairs(Players) do
+		if pPlayer:GetCivilizationType() == GameInfoTypes.CIVILIZATION_CYATS then
+			pCyats = pPlayer
+			print("Found Cyats player...")
+		end
+	end
+
+	local currentTurn = Game.GetElapsedGameTurns()
+
+
+end
+GameEvents.PlayerDoTurn.Add(CyatsActions)
 
 function ScaleConstantsBasedOnDifficulty()
 	print("Scenario constants not yet scaled by difficulty...")
