1a682b88dacb63216bb533cb2c14d8dc56cf7ba2
Stuart Holland <soulc3@gmail.com>
2013-10-19 19:29:31 +0100

    Leader flavors, civ disables, Last Battle choices.
    Added FLAVOR_LIGHT and FLAVOR_SHADOW entries for all Leaders.
    
    Tweaked the base game civ disables to use a more general system (doesn't
    require each civ to be called out separately)
    
    Added calls to Last Battle API from player Last Battle side choice
    screen.

--- WoTMod/UI/InGame/Popups/LastBattleChoiceMenu.lua
+++ WoTMod/UI/InGame/Popups/LastBattleChoiceMenu.lua
@@ -11,12 +11,16 @@ end
 GameEvents.LastBattleStart.Add(OnDisplay)
 
 function OnChooseLightCallback()
+	print("Player has chosen the Light!")
+
 	Game.ChooseLastBattleSide(Game.GetActivePlayer(), GameInfoTypes.SIDE_LIGHT)
 	ContextPtr:SetHide(true)
 end
 Controls.LightButton:RegisterCallback(Mouse.eLClick, OnChooseLightCallback)
 
 function OnChooseShadowCallback()
+	print("Player has chosen the Shadow!")
+
 	Game.ChooseLastBattleSide(Game.GetActivePlayer(), GameInfoTypes.SIDE_SHADOW)
 	ContextPtr:SetHide(true)
 end
