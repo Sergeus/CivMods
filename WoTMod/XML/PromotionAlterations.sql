9e812d1baf23411a75e2d5e0d071a829989ab9ce
Stuart Holland <soulc3@gmail.com>
2013-10-27 13:30:48 +0000

    Added early game units to tech tree. Updated some tech prereqs for new tech tree start. Added promotions that can cause a unit to attack itself.

--- WoTMod/XML/PromotionAlterations.sql
+++ WoTMod/XML/PromotionAlterations.sql
@@ -1,3 +1,4 @@
 ALTER TABLE UnitPromotions ADD TurnDamage integer DEFAULT 0;
 ALTER TABLE UnitPromotions ADD BlocksHealing boolean DEFAULT false;
 ALTER TABLE UnitPromotions ADD AdjacentEnemyDamage integer DEFAULT 0;
+ALTER TABLE UnitPromotions ADD RangedAttackSelfDamageChance integer DEFAULT 0;
---
+++ WoTMod/XML/Units/UnitsEdits.sql
@@ -0,0 +1,3 @@
+ALTER TABLE Units ADD Channeler boolean DEFAULT false;
+ALTER TABLE Units ADD UsesSaidin boolean DEFAULT false;
+ALTER TABLE Units ADD UsesSaidar boolean DEFAULT false;
