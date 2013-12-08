17f968ece7e5cb8c5fa63bd9b036cd73a0b64393
Stuart Holland <soulc3@gmail.com>
2013-12-08 17:36:40 +0000

    Switched over to new One Power blocking system.

--- WoTMod/XML/Buildings/BuildingEdits.sql
+++ WoTMod/XML/Buildings/BuildingEdits.sql
@@ -1,4 +1,3 @@
 ALTER TABLE Buildings ADD EndsWars boolean DEFAULT false;
 ALTER TABLE Buildings ADD ReligionMajorityPressureModifier integer DEFAULT 0;
 ALTER TABLE Buildings ADD UnitPurchaseCostModifier integer DEFAULT 0;
-ALTER TABLE Buildings ADD OnePowerBlockingRange integer DEFAULT -1;
