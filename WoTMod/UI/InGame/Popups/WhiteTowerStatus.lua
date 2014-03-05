7c77791e942a0068bce1b63fa3799175049b05e1
Stuart Holland <soulc3@gmail.com>
2014-03-05 21:38:14 +0000

    Working colors for Ajah influence bars. #8

--- WoTMod/UI/InGame/Popups/WhiteTowerStatus.lua
+++ WoTMod/UI/InGame/Popups/WhiteTowerStatus.lua
@@ -3,6 +3,8 @@
 -- DateCreated: 3/2/2014 7:34:30 PM
 --------------------------------------------------------------
 
+include("FLuaVector")
+
 --------------------------------------------------------------
 -- Game Logic
 --------------------------------------------------------------
@@ -49,18 +51,14 @@ function OnDisplay()
 			local iAjahPercent = pPlayer:GetAjahInfluencePercent(pAjah.ID)
 			local sAjahPercent = iAjahPercent .. "%"
 
-			local colorInfo = GameInfo.Colors[pAjah.Color]
-
-			local ajahColor = { x = colorInfo.Red, y = colorInfo.Green, z = colorInfo.Blue, w = colorInfo.Alpha }
-
-			print ("Found color " .. colorInfo.Type .. " R:" .. colorInfo.Red .. " G:" .. colorInfo.Green .. " B:" .. colorInfo.Blue .. " A:" .. colorInfo.Alpha)
+			local ajahColorInfo = GameInfo.Colors[pAjah.Color]
 
 			ContextPtr:BuildInstanceForControl("AjahInfluence", instance, Controls.InfluenceStack);
 
 			instance.AjahLabel:SetText(sAjahName)
 			instance.AjahPercent:SetText(sAjahPercent)
 			instance.InfluenceBar:SetPercent(iAjahPercent / 100)
-			instance.InfluenceBar:SetColor(ajahColor)
+			instance.InfluenceBar:SetFGColor(Color(ajahColorInfo.Red, ajahColorInfo.Green, ajahColorInfo.Blue, ajahColorInfo.Alpha))
 
 			instance.AjahStack:SetToolTipString(Locale.ConvertTextKey(pAjah.Help))
 		end
