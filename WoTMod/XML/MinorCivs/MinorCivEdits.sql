634013f11a9c121a635e3d853d0380e6c15238e5
Stuart Holland <soulc3@gmail.com>
2014-07-19 19:27:05 +0100

    Added DB columns and white tower entries to stop gold gifts and buyouts working on tar valon.

--- WoTMod/XML/MinorCivs/MinorCivEdits.sql
+++ WoTMod/XML/MinorCivs/MinorCivEdits.sql
@@ -1,2 +1,4 @@
 ALTER TABLE MinorCivTraits ADD HostsAjahs boolean DEFAULT false;
 ALTER TABLE MinorCivTraits ADD NoCoups boolean DEFAULT false;
+ALTER TABLE MinorCivTraits ADD NoBuyouts boolean DEFAULT false;
+ALTER TABLE MinorCivTraits ADD NoGoldGifts DEFAULT false;
