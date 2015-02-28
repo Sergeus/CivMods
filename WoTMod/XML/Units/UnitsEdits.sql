31bc894d3901cd61d27720db5b04ecce7d543a76
Stuart Holland <soulc3@gmail.com>
2015-02-28 17:47:27 +0000

    Added DB entries for bonding Warder mission

--- WoTMod/XML/Units/UnitsEdits.sql
+++ WoTMod/XML/Units/UnitsEdits.sql
@@ -1,2 +1,4 @@
 ALTER TABLE Builds ADD Happiness integer DEFAULT 0;
 ALTER TABLE Builds ADD OwnTerritoryMakesValid boolean DEFAULT false;
+
+ALTER TABLE Units ADD BondsWarders boolean DEFAULT false;
