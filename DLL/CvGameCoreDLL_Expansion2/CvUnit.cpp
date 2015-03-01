7548da2e6fc5f858ccb54c346bb1dfb5c0c0c556
Stuart Holland <soulc3@gmail.com>
2015-03-01 17:30:11 +0000

    Removed mission ID from custom mission Lua subscriber parameters, since it's no longer needed to differentiate missions.

--- DLL/CvGameCoreDLL_Expansion2/CvUnit.cpp
+++ DLL/CvGameCoreDLL_Expansion2/CvUnit.cpp
@@ -3907,7 +3907,6 @@ bool CvUnit::CanHandleMission(MissionTypes eMission, int iData1, int iData2, CvP
 
 			args->Push(getOwner());
 			args->Push(GetID());
-			args->Push(eMission);
 			args->Push(bTestVisible);
 
 			bool bResult;
@@ -3946,7 +3945,6 @@ bool CvUnit::HandleMission(MissionTypes eMission)
 
 			args->Push(getOwner());
 			args->Push(GetID());
-			args->Push(eMission);
 
 			bool bResult;
 			if (LuaSupport::CallHook(pkScriptSystem, luaEventName, args.get(), bResult))
